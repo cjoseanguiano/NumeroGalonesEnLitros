b3ee34795b1ad6d815bd8b0c3910465be9b59611
Carlos Anguiano <carlos.anguiano@gmail.com>
2017-03-10 23:41:57 -0600

    2

--- src/com/generarenteros/company/Main.java
+++ src/com/generarenteros/company/Main.java
@@ -13,23 +13,38 @@ public class Main {
     public static void main(String[] args) {
 
         double dameLitro;
+        double galonConvertir;
         double total;
+        double migalon;
 
-        dameLitro = Double.parseDouble(JOptionPane.showInputDialog("Dame el litro a convertir"));
-
+        dameLitro = Double.parseDouble(JOptionPane.showInputDialog("Dame el litro a convertir a galon"));
+//7.5708
         if (dameLitro > 0) {
             total = dameLitro * litro;
             JOptionPane.showMessageDialog(null, "Valor " + total);
+        } else {
+//            galonConvertir = Double.parseDouble(JOptionPane.showInputDialog("Dame el galon a convertir a litro"));
+            calcularLitroaGalon();
+
         }
     }
 
-    public static double calcularLitroaGalon(double litro) {
-        int litroa = 0;
-        if (litro > 0) {
-            
-        } else {
-            JOptionPane.showMessageDialog(null, "Ingresa un valor valido");
+    public static void calcularLitroaGalon() {
+//7.5708
+// 3.7854
+//11.3562
+        int contador = 0;
+        double valore = (double) (3.7854 * 3);
+        System.out.println("MY VALOR " +valore);
+        for (int i = 1; i < 10; i++) {
+            if (11.3562 / i == 3.7853999999999997) {
+                System.out.println("---SI---");
+                contador++;
+            } else {
+                System.out.println("---NO---");
+            }
         }
-        return litroa;
+
+
     }
 }
